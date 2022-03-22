278ece6402d8f41da85e159276ef99ba5f264235
Pawel Gorgolewski <pgorgolewski01@gmail.com>
2022-03-22 15:22:31 +0000

    lib and sys but not working properly

--- lab2/zad1/main.c
+++ lab2/zad1/main.c
@@ -6,30 +6,41 @@
 
 int MAX_LINE_LEN = 256;
 
-int write_with_sys(char* to_copy_file, char* result_file){
+int write_without_black(char* to_copy_file, char* result_file){
     char* buffor = calloc(MAX_LINE_LEN, sizeof(char));
-    int out, reading_file, writing_file;
+    int out;
 
-    reading_file = open(to_copy_file, O_RDONLY);
-    writing_file = open(result_file, O_WRONLY|O_CREAT, S_IRUSR|S_IWUSR);
+    #ifdef LIB
+        FILE* reading_file = fopen(to_copy_file, "r");
+        FILE* writing_file = fopen(result_file, "w+");
+        out=fread(buffor, sizeof(char), MAX_LINE_LEN, reading_file);
+    #else
+        int reading_file = open(to_copy_file, O_RDONLY);
+        int writing_file = open(result_file, O_WRONLY|O_CREAT, S_IRUSR|S_IWUSR);
+        out=read(reading_file, buffor, sizeof(buffor));
+    #endif
+
 
     //do reading and writing
-    out=read(reading_file, buffor, sizeof(buffor));
+    int is_new_line = 0;
     while(out > 0){
         char* line_to_write = calloc(MAX_LINE_LEN+2, sizeof(char));
-        int is_new_line, new_line_index = 0;
+        int new_line_index = 0;
         for (int i=0; i<out; i++){
-            if (is_new_line == 1 && buffor[i] == '\n'){
-                line_to_write[i-new_line_index] = '\n';
+            if (i-new_line_index == 0 && buffor[i] == '\n'){
+                new_line_index = i+1;
+            }
+            else if (is_new_line == 1 && buffor[i] == '\n'){
                 line_to_write[i-new_line_index+1] = '\0';
 
-                write(writing_file, line_to_write, sizeof(line_to_write));
+                #ifdef LIB
+                    fwrite(line_to_write, sizeof(char), i-new_line_index+1, writing_file);
+                #else
+                    write(writing_file, line_to_write, i-new_line_index+1);
+                #endif
+                
                 new_line_index = i+1;
-                is_new_line = 0;
-            }
-            else if (i-new_line_index == 0 && buffor[i] == '\n'){
-                new_line_index = i+1;
-            }
+            } 
             else{
                 line_to_write[i-new_line_index] = buffor[i];
 
@@ -39,19 +50,31 @@ int write_with_sys(char* to_copy_file, char* result_file){
                     is_new_line = 0;
             }
         }
-        printf("%s\n", buffor);
-        out=read(reading_file, buffor, sizeof(buffor));
+        printf("%s", line_to_write);
+        line_to_write[(out-1)-new_line_index+1] = '\n';
+
+        #ifdef LIB
+            fwrite(line_to_write, sizeof(char), out-new_line_index, writing_file);
+            out=fread(buffor, sizeof(char), MAX_LINE_LEN, reading_file);
+        #else
+            write(writing_file, line_to_write, out-new_line_index);
+            out=read(reading_file, buffor, sizeof(buffor));
+        #endif
+
         free(line_to_write);
     }
     free(buffor);
 
-    // close here
-    close(reading_file);
-    close(writing_file);
+    #ifdef LIB
+        fclose(reading_file);
+        fclose(writing_file);
+    #else
+        close(reading_file);
+        close(writing_file);
+    #endif
 
     return 0;
 }
-
 
 int main(int arg_len, char **args){
     //first arg is './main'
@@ -60,33 +83,27 @@ int main(int arg_len, char **args){
         return -1;
     }
     
-    char* to_copy_file = calloc(MAX_LINE_LEN, sizeof(char));
-    char* result_file = calloc(MAX_LINE_LEN, sizeof(char));
+    char* to_copy_file;
+    char* result_file;
 
     if (arg_len < 2) {
+        to_copy_file = calloc(MAX_LINE_LEN, sizeof(char));
         printf("Please enter the to_copy_file name:\n");
         scanf("%s", to_copy_file);
     }
-    else {
-        to_copy_file = args[2];
-    }
+    else
+        to_copy_file = args[1];
 
     if (arg_len < 3) {
+        result_file = calloc(MAX_LINE_LEN, sizeof(char));
         printf("Please enter the result_file name:\n");
         scanf("%s", result_file);
     }
-    else {
-        result_file = args[3];
-    }
+    else 
+        result_file = args[2];
 
     printf("Loaded files are: to_copy_file -> %s and result_file -> %s\n", to_copy_file, result_file);
-    
-    #ifdef LIB
-        write_with_sys(to_copy_file, result_file);
-    #endif
-    #ifndef LIB
-        write_with_sys(to_copy_file, result_file);
-    #endif
+    write_without_black(to_copy_file, result_file);
 
     return 0;
 }
