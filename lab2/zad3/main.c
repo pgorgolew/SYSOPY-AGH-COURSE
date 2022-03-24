595a0cdaa1e182433d135f58354ea8324d0ef0ff
Pawel Gorgolewski <pgorgolewski01@gmail.com>
2022-03-24 15:31:45 +0000

    3a done

--- lab2/zad3/main.c
+++ lab2/zad3/main.c
@@ -5,8 +5,29 @@
 #include <unistd.h>
 #include <time.h>
 #include <stdlib.h>
+#include <string.h>
 
 int MAX_PATH_LEN = 256;
+
+char* path_without_last_dir(char* cwd){
+    int last_index = strlen(cwd) - 1;
+    while (cwd[last_index] != '/')
+        last_index--;
+    
+    cwd[last_index] = '\0';
+    return cwd;
+}
+
+struct counter{
+    int files;
+    int dirs;
+    int fifos;
+    int sockets;
+    int char_devs;
+    int block_devs;
+    int links;
+    int unknows;
+};
 
 char* get_date(long int time_in_sec){
     char* date = calloc(20, sizeof(char));
@@ -14,7 +35,40 @@ char* get_date(long int time_in_sec){
     return date;
 }
 
-int listFiles(const char* dirname) {
+char* get_type(int type, struct counter* c){
+    if (type == DT_DIR){
+        c->dirs++;
+        return "Directory";
+    }
+    else if (type==DT_REG){
+        c->files++;
+        return "Regular file";    
+    }
+    else if (type == DT_FIFO){
+        c->fifos++;
+        return "FIFO\t";
+    }
+    else if (type == DT_SOCK){
+        c->sockets++;
+        return "SOCKET\t";
+    }
+    else if (type == DT_CHR){
+        c->char_devs++;
+        return "Character device";
+    }
+    else if (type == DT_BLK){
+        c->block_devs++;
+        return "Block device";
+    }    
+    else if (type == DT_LNK){
+        c->links++;
+        return "LINK\t";
+    }
+    c->unknows++;
+    return "Unknown\t";
+}
+
+int listFiles(const char* dirname, struct counter* c) {
     DIR* dir = opendir(dirname);
     if (dir == NULL) {
         printf("ERROR: cannot open dir: %s\n", dirname);
@@ -43,8 +97,9 @@ int listFiles(const char* dirname) {
             return 1;
         }
 
-        char* type = "file";
-        printf("%lu\t%s\t%ld\t%s\t%s\t%s/%s\n", entity_stat.st_nlink, type, entity_stat.st_size, get_date(entity_stat.st_atime), get_date(entity_stat.st_mtime), dirname, entity->d_name);
+        char* type = get_type(entity->d_type, c);
+        printf("%lu\t%s\t%ld\t%s\t%s\t%s/%s\n", entity_stat.st_nlink, type, entity_stat.st_size, get_date(entity_stat.st_atime), 
+                get_date(entity_stat.st_mtime), dirname, entity->d_name);
         if (entity->d_type == DT_DIR) {
             char path[MAX_PATH_LEN];
             int snprintf_result = snprintf(path, MAX_PATH_LEN, "%s/%s", dirname, entity->d_name);
@@ -54,7 +109,7 @@ int listFiles(const char* dirname) {
                 return 1;
             }
 
-            listFiles(path);
+            listFiles(path, c);
         }
         entity = readdir(dir);
     }
@@ -75,14 +130,27 @@ int main(int argc, char* argv[]) {
             printf("ERROR: getcwd() error");
             return 1;
         }
-        snprintf_result = snprintf(total_path, MAX_PATH_LEN, "%s/%s", cwd, argv[1]);
-    }
-        
-    if (snprintf_result < 0 || snprintf_result > MAX_PATH_LEN){
-    printf("ERROR: snprintf error, return %d", snprintf_result);
-    return 1;
-    }
 
-    listFiles(total_path);
+        if (strcmp(argv[1], ".") == 0)
+            snprintf_result = snprintf(total_path, MAX_PATH_LEN, "%s", cwd);
+        else if (strcmp(argv[1], "..") == 0)
+            snprintf_result = snprintf(total_path, MAX_PATH_LEN, "%s", path_without_last_dir(cwd));
+        else  
+            snprintf_result = snprintf(total_path, MAX_PATH_LEN, "%s/%s", cwd, argv[1]);
+    }
+    
+    printf("total_path -> %s    GIVEN -> %s\n\n", total_path, argv[1]);
+    if (snprintf_result < 0 || snprintf_result > MAX_PATH_LEN){
+        printf("ERROR: snprintf error, return %d", snprintf_result);
+        return 1;
+    }
+    struct counter c = {0,0,0,0,0,0,0,0};
+    struct counter* ptr_c = &c;
+
+    printf("NLINKS\tTYPE\t\tSIZE\tLAST_ACCES\t\tLAST_MODIFICATION\tABS_PATH\n");
+    listFiles(total_path, ptr_c);
+    printf("TOTALS:\n");
+    printf("DIR: %d   FILE: %d   FIFO: %d   SOCKET: %d   CHAR_DEV: %d   BLOCK_DEV: %d   LINK: %d   UNKNOW: %d\n", 
+           c.dirs, c.files, c.fifos, c.sockets, c.char_devs, c.block_devs, c.links, c.unknows);
     return 0;
 }
