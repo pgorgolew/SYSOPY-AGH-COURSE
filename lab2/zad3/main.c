0b1ce4de24fedd83108d871734ad92fcfbcc489b
Pawel Gorgolewski <pgorgolewski01@gmail.com>
2022-03-24 17:13:07 +0000

    nftw done

--- lab2/zad3/main.c
+++ lab2/zad3/main.c
@@ -1,3 +1,6 @@
+#define _XOPEN_SOURCE 500
+#define _DEFAULT_SOURCE
+#include <ftw.h>
 #include <stdio.h>
 #include <string.h>
 #include <dirent.h>
@@ -5,18 +8,8 @@
 #include <unistd.h>
 #include <time.h>
 #include <stdlib.h>
-#include <string.h>
+#include <stdint.h>
 
-int MAX_PATH_LEN = 256;
-
-char* path_without_last_dir(char* cwd){
-    int last_index = strlen(cwd) - 1;
-    while (cwd[last_index] != '/')
-        last_index--;
-    
-    cwd[last_index] = '\0';
-    return cwd;
-}
 
 struct counter{
     int files;
@@ -29,46 +22,66 @@ struct counter{
     int unknows;
 };
 
+struct counter c = {0,0,0,0,0,0,0,0};
+struct counter* ptr_c = &c;
+int MAX_PATH_LEN = 256;
+
+char* path_without_last_dir(char* cwd){
+    int last_index = strlen(cwd) - 1;
+    while (cwd[last_index] != '/')
+        last_index--;
+    
+    cwd[last_index] = '\0';
+    return cwd;
+}
+
 char* get_date(long int time_in_sec){
     char* date = calloc(20, sizeof(char));
     strftime(date, 20, "%Y-%m-%d %H:%M:%S", localtime(&time_in_sec));
     return date;
 }
 
-char* get_type(int type, struct counter* c){
-    if (type == DT_DIR){
-        c->dirs++;
+char* get_type(mode_t mode){
+    if (S_ISDIR(mode)){
+        c.dirs++;
         return "Directory";
     }
-    else if (type==DT_REG){
-        c->files++;
+    else if (S_ISREG(mode)){
+        c.files++;
         return "Regular file";    
     }
-    else if (type == DT_FIFO){
-        c->fifos++;
+    else if (S_ISFIFO(mode)){
+        c.fifos++;
         return "FIFO\t";
     }
-    else if (type == DT_SOCK){
-        c->sockets++;
+    else if (S_ISSOCK(mode)){
+        c.sockets++;
         return "SOCKET\t";
     }
-    else if (type == DT_CHR){
-        c->char_devs++;
+    else if (S_ISCHR(mode)){
+        c.char_devs++;
         return "Character device";
     }
-    else if (type == DT_BLK){
-        c->block_devs++;
+    else if (S_ISBLK(mode)){
+        c.block_devs++;
         return "Block device";
     }    
-    else if (type == DT_LNK){
-        c->links++;
+    else if (S_ISLNK(mode)){
+        c.links++;
         return "LINK\t";
     }
-    c->unknows++;
+    c.unknows++;
     return "Unknown\t";
 }
 
-int listFiles(const char* dirname, struct counter* c) {
+int parse_nftw_res(const char* filename, const struct stat* statptr, int tflag, struct FTW* ftwbuf){
+    char* type = get_type(statptr->st_mode);
+    printf("%lu\t%s\t%ld\t%s\t%s\t%s\n", statptr->st_nlink, type, statptr->st_size, get_date(statptr->st_atime), 
+            get_date(statptr->st_mtime), filename);
+    return 0;
+}
+
+int listFiles(const char* dirname) {
     DIR* dir = opendir(dirname);
     if (dir == NULL) {
         printf("ERROR: cannot open dir: %s\n", dirname);
@@ -97,7 +110,7 @@ int listFiles(const char* dirname, struct counter* c) {
             return 1;
         }
 
-        char* type = get_type(entity->d_type, c);
+        char* type = get_type(entity_stat.st_mode);
         printf("%lu\t%s\t%ld\t%s\t%s\t%s/%s\n", entity_stat.st_nlink, type, entity_stat.st_size, get_date(entity_stat.st_atime), 
                 get_date(entity_stat.st_mtime), dirname, entity->d_name);
         if (entity->d_type == DT_DIR) {
@@ -109,7 +122,7 @@ int listFiles(const char* dirname, struct counter* c) {
                 return 1;
             }
 
-            listFiles(path, c);
+            listFiles(path);
         }
         entity = readdir(dir);
     }
@@ -144,11 +157,15 @@ int main(int argc, char* argv[]) {
         printf("ERROR: snprintf error, return %d", snprintf_result);
         return 1;
     }
-    struct counter c = {0,0,0,0,0,0,0,0};
-    struct counter* ptr_c = &c;
 
     printf("NLINKS\tTYPE\t\tSIZE\tLAST_ACCES\t\tLAST_MODIFICATION\tABS_PATH\n");
-    listFiles(total_path, ptr_c);
+
+    #ifdef NFTW
+        nftw(total_path, parse_nftw_res, 20, 0);
+    #else
+        listFiles(total_path);
+    #endif
+
     printf("TOTALS:\n");
     printf("DIR: %d   FILE: %d   FIFO: %d   SOCKET: %d   CHAR_DEV: %d   BLOCK_DEV: %d   LINK: %d   UNKNOW: %d\n", 
            c.dirs, c.files, c.fifos, c.sockets, c.char_devs, c.block_devs, c.links, c.unknows);
